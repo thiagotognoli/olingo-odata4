65b30b0ebdd3b559bd0ae6b50bdc3d0c3feeb9bb
thiagotognoli <code@togger.com.br>
2025-08-02 17:06:22 -0300

    bugfix test

--- fit/src/test/java/org/apache/olingo/fit/tecsvc/http/ODataVersionConformanceITCase.java
+++ fit/src/test/java/org/apache/olingo/fit/tecsvc/http/ODataVersionConformanceITCase.java
@@ -42,7 +42,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     URL url = new URL(SERVICE_URI + "ESAllPrim");
 
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod(HttpMethod.GET.name()); 
+    connection.setRequestMethod(HttpMethod.GET.name());
     connection.setRequestProperty(HttpHeader.ODATA_VERSION, "3.0");
     connection.connect();
 
@@ -52,7 +52,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
     assertTrue(content.contains("OData version '3.0' is not supported."));
   }
-  
+
   @Test
   public void invalidODataVersionHeader2() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim");
@@ -68,7 +68,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
     assertTrue(content.contains("OData version '5.0' is not supported."));
   }
-  
+
   @Test
   public void invalidODataMaxVersionHeader1() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim");
@@ -84,7 +84,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
     assertTrue(content.contains("OData version '3.0' is not supported."));
   }
-  
+
   @Test
   public void validODataMaxVersionHeader() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim");
@@ -94,9 +94,10 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     connection.setRequestProperty(HttpHeader.ODATA_MAX_VERSION, "5.0");
     connection.connect();
 
+    assertEquals(HttpStatusCode.OK.getStatusCode(), connection.getResponseCode());
     assertEquals("4.0", connection.getHeaderField(HttpHeader.ODATA_VERSION));
 
-    final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
+    final String content = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
     assertNotNull(content);
   }
 
@@ -110,12 +111,13 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     connection.setRequestProperty(HttpHeader.ODATA_MAX_VERSION, "5.0");
     connection.connect();
 
+    assertEquals(HttpStatusCode.OK.getStatusCode(), connection.getResponseCode());
     assertEquals("4.0", connection.getHeaderField(HttpHeader.ODATA_VERSION));
 
-    final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
-    assertNotNull(content);;
+    final String content = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+    assertNotNull(content);
   }
-  
+
   @Test
   public void validODataVersionAndMaxVersionHeader1() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim?$format=json");
@@ -134,7 +136,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
     assertNotNull(content);
   }
-  
+
   @Test
   public void validODataVersionAndMaxVersionHeader2() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim?$format=json");
@@ -153,7 +155,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
     assertNotNull(content);
   }
-  
+
   @Test
   public void invalidODataVersionAndMaxVersionHeader() throws Exception {
     URL url = new URL(SERVICE_URI + "ESAllPrim");
@@ -170,7 +172,7 @@ public class ODataVersionConformanceITCase extends AbstractBaseTestITCase {
     final String content = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
     assertTrue(content.contains("OData version '5.0' is not supported."));
   }
-  
+
   @Override
   protected ODataClient getClient() {
     return null;
